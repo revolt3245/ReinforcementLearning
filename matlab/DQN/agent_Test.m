eda70550cfe20ddda6991a71db92ba2efc7a25df
revolt3245 <44830134+revolt3245@users.noreply.github.com>
2021-05-02 15:57:33 +0900

    result act/ops

--- matlab/DQN/agent_Test.m
+++ matlab/DQN/agent_Test.m
@@ -11,12 +11,33 @@ simOptions = rlSimulationOptions('MaxSteps', 500);
 experience1 = sim(env1, agent1, simOptions);
 experience2 = sim(env2, agent2, simOptions);
 
-hold on;
-figure(3);
+%% env1
+f1 = figure(3);
 plot(experience1.Observation.CartPoleStates);
 h1 = legend("$x$", "$\dot{x}$", "$\theta$", "$\dot{\theta}$");
 h1.Interpreter = 'latex';
-figure(4);
+title("Observation of 2-action-state environment");
+
+f2 = figure(4);
+plot(experience1.Action.CartPoleAction);
+h1 = legend("$F$");
+h1.Interpreter = 'latex';
+title("Action of 2-action-state environment");
+
+%%env2
+f3 = figure(5);
 plot(experience2.Observation.CartPoleStates);
 h2 = legend("$x$", "$\dot{x}$", "$\theta$", "$\dot{\theta}$");
 h2.Interpreter = 'latex';
+title("Observation of 5-action-state environment");
+
+f4 = figure(6);
+plot(experience2.Action.CartPoleAction);
+h2 = legend("$F$");
+h2.Interpreter = 'latex';
+title("Action of 5-action-state environment");
+
+saveas(f1, "ExperienceResult\trial1\2state_obs.png");
+saveas(f2, "ExperienceResult\trial1\2state_act.png");
+saveas(f3, "ExperienceResult\trial1\5state_obs.png");
+saveas(f4, "ExperienceResult\trial1\5state_act.png");
