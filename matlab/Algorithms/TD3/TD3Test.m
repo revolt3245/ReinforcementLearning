d634a27784c3a9bae652b8d4c14d491c94020e01
revolt3245 <44830134+revolt3245@users.noreply.github.com>
2021-05-07 17:26:12 +0900

    Train TD3 Algorithm

--- matlab/Algorithms/TD3/TD3Test.m
+++ matlab/Algorithms/TD3/TD3Test.m
@@ -1,2 +1,81 @@
 clear; clc; close;
 
+if ~isfile('CartPoleContinuousAction2.m')
+    copyfile('..\..\CustomEnvironment\CartPole\CartPoleContinuousAction2.m', pwd);
+end
+
+Env = CartPoleContinuousAction2;
+
+ObsInfo = Env.getObservationInfo;
+ActInfo = Env.getActionInfo;
+
+rng(1);
+
+%% Critic Network
+StatePath = [
+    featureInputLayer(ObsInfo.Dimension(1), 'Name', 'ObsInput', 'Normalization', 'none')
+    fullyConnectedLayer(24, 'Name', 'ObsL1')
+    reluLayer('Name', 'ObsReLU1')
+    fullyConnectedLayer(24, 'Name', 'ObsL2')
+    ];
+
+ActionPath = [
+    featureInputLayer(ActInfo.Dimension(1), 'Name', 'ActInput', 'Normalization', 'none')
+    fullyConnectedLayer(24, 'Name', 'ActL1', 'BiasLearnRateFactor', 0)
+    ];
+
+CommonPath = [
+    additionLayer(2, 'Name', 'Add')
+    reluLayer('Name', 'CommonReLU')
+    fullyConnectedLayer(1, 'Name', 'CriticOutput')
+    ];
+
+CriticNetwork = layerGraph(StatePath);
+CriticNetwork = CriticNetwork.addLayers(ActionPath).addLayers(CommonPath);
+CriticNetwork = CriticNetwork.connectLayers('ObsL2', 'Add/in1').connectLayers('ActL1', 'Add/in2');
+
+
+CriticOption = rlRepresentationOptions('LearnRate', 1e-3, 'GradientThreshold', 1, 'UseDevice', 'gpu');
+Critic = rlQValueRepresentation(CriticNetwork, ObsInfo, ActInfo,...
+    'Observation', {'ObsInput'}, 'Action', {'ActInput'}, CriticOption);
+
+%% Actor Network
+ActorNetwork = [
+    featureInputLayer(ObsInfo.Dimension(1), 'Name', 'ActorInput')
+    fullyConnectedLayer(24, 'Name', 'ActorL1')
+    reluLayer('Name', 'ActorReLU1')
+    fullyConnectedLayer(24, 'Name', 'ActorL2')
+    reluLayer('Name', 'ActorReLU2')
+    fullyConnectedLayer(ActInfo.Dimension(1), 'Name', 'ActorL3')
+    tanhLayer('Name', 'ActorTanh1')
+    scalingLayer('Name', 'ActorScaling', 'Scale', ActInfo.UpperLimit)
+    ];
+
+ActorOption = rlRepresentationOptions('LearnRate', 5e-4, 'GradientThreshold', 1, 'UseDevice', 'gpu');
+
+Actor = rlDeterministicActorRepresentation(ActorNetwork, ObsInfo, ActInfo,...
+    'Observation', {'ActorInput'}, 'Action', {'ActorScaling'}, ActorOption);
+
+%% Agent
+AgentOptions = rlTD3AgentOptions(...
+    'SampleTime', Env.Ts,...
+    'TargetSmoothFactor', 5e-2,...
+    'ExperienceBufferLength', 1e6,...
+    'MiniBatchSize', 256);
+
+Agent = rlTD3Agent(Actor, Critic, AgentOptions);
+
+%% Train
+TrainOpts = rlTrainingOptions(...
+    'MaxEpisodes',10000, ...
+    'MaxStepsPerEpisode',500, ...
+    'Verbose',false, ...
+    'Plots','training-progress',...
+    'StopTrainingCriteria','AverageReward',...
+    'StopTrainingValue',480);
+
+plot(Env);
+TrainingStats = train(Agent, Env, TrainOpts);
+
+save('agent_saver.mat', 'Agent');
+save('train_res.mat', 'TrainingStats');
